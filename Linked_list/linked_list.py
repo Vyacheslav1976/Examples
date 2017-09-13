73f7c0d9402d54d876ab794cc797ca3094dd1235
Veygandt Vyacheslav <veysl@mail.ru>
2017-09-13 22:41:09 +0500

    Commit Linked_list directory+file

--- Linked_list/linked_list.py
+++ Linked_list/linked_list.py
@@ -1,1 +1,15 @@
 print("Hello linkedlist")
+
+class Nod:
+    def __init__(self, data, next):
+        self.data = data
+	selfnext = next
+
+
+class Linked_list(object):
+    """docstring"""
+    
+    def __init__(self):
+        """Constructor"""
+        pass
+ 
